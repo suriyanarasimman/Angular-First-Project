ad1bb77dc86f8d24f557e3ddadec0a55ce36d918
suriyanarasimman <9444533098me@gmail.com>
2021-10-25 12:51:54 +0530

    New componet named servers created using CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ServerComponent} from './server/server.component'
 import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
