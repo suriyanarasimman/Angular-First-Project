54176a8c269fc2aac560f35ab22c36bdd4ab05fa
suriyanarasimman <9444533098me@gmail.com>
2021-10-25 12:39:07 +0530

    Manually added a Componet called Server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {ServerComponent} from './server/server.component'
 import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
